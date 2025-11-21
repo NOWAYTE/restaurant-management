2f04eb289f94c61544ddf4beb94b06f04fe84cba
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2025-11-21 13:16:35 +0300

    Update

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,8 +1,9 @@
+from app.extensions import db
 from .reservation import Reservation
 from .menu_item import MenuItem
 from .inventory_item import InventoryItem
 from .order import Order, OrderItem
 from .user import User
 
-__all__ = ['Reservation', 'MenuItem', 'InventoryItem', 'Order', 'OrderItem', 'User']
+__all__ = ['db', 'Reservation', 'MenuItem', 'InventoryItem', 'Order', 'OrderItem', 'User']
 
