cc5b8bec6a8d623b9daa8c16bcd43017931da668
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2025-11-25 19:04:03 +0300

    Remove authorization header

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -25,13 +25,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("config")
 
-    CORS(resources={
-             r"/api/*": {
-                 "origins": ["http://localhost:3000"],  # Your frontend URL
-                 "supports_credentials": True,
-                 "allow_headers": ["Content-Type", "Authorization"]
-             }
-         })
+    CORS(app)
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
